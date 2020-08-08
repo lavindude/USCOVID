55abb52a28238e946f4003dfc9ad11f6579764e7
lavindude <lavindude@gmail.com>
2020-08-08 08:50:34 -0700

    27

--- algorithm.php
+++ algorithm.php
@@ -1,14 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="styleAlg.css">    
+</head>
+<body>
+<p>Welcome to my COVID map! I hope you are safe and healthy.</p>
+<img src="graph.png">
+
+</html>
 <?php
-    echo('style="background-color: #00bcff;"');
-    echo("Welcome to my COVID map! I hope you are safe and healthy. ");
-    echo("<br><br>");
-    echo('<img src="graph.png">');
     echo("Red means that the the slope of the graph above is greater than 50.<br>");
     echo("Yellow means that the the slope of the graph above is between 0 and 50.<br>");
-    echo("Green means that the the slope of the graph above is below 0.<br>");
-    echo("<br><br>");
+    echo("Green means that the the slope of the graph above is below 0.<br><br>");
     echo("Linear Regression is used to find the slope and thus predict the trend by<br>");
     echo("averaging all the points.");
 
     echo('<p><a href="./">Back to US Map</a></p>');
 ?>
+</body>
