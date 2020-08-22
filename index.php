b36e2a3466c4862396e8037b96aa28ce40131c63
lavindude <lavindude@gmail.com>
2020-08-22 08:15:41 -0700

    c89

--- index.php
+++ index.php
@@ -1347,8 +1347,8 @@
         </svg>
     </div>
     <p style="position: relative; top: -1450px">Updated every day at 6:00 am PST.</p>
-    <p style="position: relative; top: -1440px">Source: <a href="https://covidtracking.com/api/v1/states/current.json">https://covidtracking.com/api/v1/states/current.json</a></p>
-    <p style="position: relative; top: -1430px">If you have any questions, comments, or concerns, please email me at lavindude@gmail.com</p>
+    <p style="position: relative; top: -1445px">Source: <a href="https://covidtracking.com/api/v1/states/current.json">https://covidtracking.com/api/v1/states/current.json</a></p>
+    <p style="position: relative; top: -1440px">If you have any questions, comments, or concerns, please email me at lavindude@gmail.com</p>
 </body>
 
 </html>
