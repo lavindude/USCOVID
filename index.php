f34a9666d9e669fd2052d3d295ed3bfa87295701
lavindude <lavindude@gmail.com>
2020-08-23 20:17:32 -0700

    c74

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
         height="1730" width="1700"
         version="1.1"
         viewBox="0 0 1000 589"
-        style="position: relative; left: 140px; top: auto;"
+        style="position: relative; left: auto; top: auto;"
         >
         <defs>
             <style type="map/css/style.css"> </style>
