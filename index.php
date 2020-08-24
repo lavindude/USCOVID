c251fc652af148666b3ce26b33d22cd998929aa0
lavindude <lavindude@gmail.com>
2020-08-24 09:25:07 -0700

    last

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
         <div class="container">
             <nav>
                 <ul>
-                    <li><a href="index.php"><b>Map</b></a></li>
+                    <li style="color: #000000;"><a href="index.php">Map</a></li>
                     <li><a href="algorithm.html">Algorithm</a></li>
                     <li><a href="contact.html">Contact</a></li>
                 </ul>
