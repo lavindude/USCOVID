e34012799a1efc75f20f6ae4cc2a7aa9b2741c83
lavindude <lavindude@gmail.com>
2020-08-23 21:29:21 -0700

    c74

--- index.php
+++ index.php
@@ -38,10 +38,10 @@
     <div class="mapdiv">
         <svg
         xmlns="http://www.w3.org/2000/svg"
-        height="1230" width="1700"
+        height="930" width="1700"
         version="1.1"
         viewBox="0 0 1000 589"
-        style="position: relative; left: 140px; top: -670px"
+        style="position: relative; left: 140px; top: -470px"
         >
         <defs>
             <style type="map/css/style.css"> </style>
