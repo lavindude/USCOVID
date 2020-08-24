ff5b5687439c4141ac466a95221ecf64b81b75cd
lavindude <lavindude@gmail.com>
2020-08-23 21:23:30 -0700

    c74

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <link href="https://fonts.googleapis.com/css?family=Work+Sans:400,600">
     <header>
         <div class="container">
-            <nav style="position: relative; left: -500px;">
+            <nav style="position: relative; left: -900px;">
                 <ul>
                     <li><a href="index.php">Map</a></li>
                     <li><a href="algorithm.html">Algorithm</a></li>
