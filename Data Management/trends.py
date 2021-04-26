2a15a3132760039a83f13df72232709aaae7a83f
lavindude <60680607+lavindude@users.noreply.github.com>
2021-04-26 13:47:49 -0700

    Update trends.py

--- Data Management/trends.py
+++ Data Management/trends.py
@@ -2,7 +2,7 @@ import json
 import http.client
 import sqlite3
 import time
-from pgdb import connect
+from pgdb import Connection
 import urllib.request, urllib.parse, urllib.error
 import psycopg2
 from datetime import date
@@ -17,36 +17,30 @@ import numpy as np
 # import mpld3
 
 #python database connection
-conn1 = connect(****)
+conn1 = Connection(*****)
 cur = conn1.cursor()
 
 #pandas database connection (used for linear regression)
-engine = sqlalchemy.create_engine(*****)
+engine = sqlalchemy.create_engine(******)
 covid_data = pd.read_sql_table("covid", engine)
 
 cur.execute('SELECT MAX(state_data_id) FROM covid;')
 count = cur.fetchone()[0]
 
-codes = ["AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DE", "FL", "GA", "HI", 
-"IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", 
-"MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", 
-"OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VT", "WA", 
-"WI", "WV", "WY"]
-
 today = date.today()
 today = str(today)
-state_id = 1
-for code in codes:
+
+for i in range(1, 51):
     x = list()
     y = list()
 
-    cur.execute('SELECT positiveincrease FROM covid WHERE code=%s', [code])
+    cur.execute('SELECT positiveincrease FROM covid WHERE state_id=%s', [i])
     p_increase_data = list(cur.fetchall())
 
     day = 1
     for data in p_increase_data:
         if data[0] == 0:
-            continue
+          continue
         x.append(day)
         y.append(data[0])
         day += 1  
@@ -58,43 +52,35 @@ for code in codes:
     linear = linear_model.LinearRegression(fit_intercept=True)
     linear.fit(x, y)
     y_pred = linear.predict(x)
-    plt.plot(x, y_pred, color='red')
-    plt.scatter(x, y)
-    
-    # uncomment to see graph:
+    # plt.plot(x, y_pred, color='red')
+    # plt.scatter(x, y)
     # plt.xlabel('Day')
     # plt.ylabel('Positive Increase')
-    # plt.show()
-
-    cur.execute('SELECT state_id FROM covid WHERE code=%s', [code])
-    state_id = cur.fetchone()[0]
+    #plt.show()
 
     m = linear.coef_
     
-    cur.execute('SELECT positive FROM covid WHERE state_id=%s ORDER BY date DESC', [state_id])
+    cur.execute('SELECT positive FROM covid WHERE state_id=%s ORDER BY date DESC', [i])
     positive = cur.fetchone()[0]
     
-    cur.execute('SELECT population FROM states WHERE state_id=%s', [state_id])
+    cur.execute('SELECT population FROM states WHERE state_id=%s', [i])
     population = cur.fetchone()[0]
 
     #casesDivPop score algorithm:
     posDivPop_score = positive / population
     print(posDivPop_score)
 
-    print('The slope is ', m, 'for code =', code)
+    print('The slope is ', m, 'for state_id =', i)
     if m > 25 and posDivPop_score > 0.01:
-        cur.execute("UPDATE states SET color='r' WHERE state_id=%s", [state_id])
+        cur.execute("UPDATE states SET color='r' WHERE state_id=%s", [i])
         print("Red")
     else:
       if m >= 0 and m < 50 and posDivPop_score > 0.007:
-        cur.execute("UPDATE states SET color='y' WHERE state_id=%s", [state_id])
+        cur.execute("UPDATE states SET color='y' WHERE state_id=%s", [i])
         print("Yellow")
       else:
-        cur.execute("UPDATE states SET color='g'WHERE state_id=%s", [state_id])
+        cur.execute("UPDATE states SET color='g'WHERE state_id=%s", [i])
         print("Green")
-
-    state_id += 1
-    continue
 
 
 conn1.commit()
